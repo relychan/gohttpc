ac98d35a6068908b8161b864e3145e24651b1fde
Toan Nguyen <hgiasac@gmail.com>
2025-11-30 21:14:01 +0700

    emit the initial state

--- loadbalancer/health.go
+++ loadbalancer/health.go
@@ -290,9 +290,12 @@ func (hb *httpHealthCheckPolicyBuilder) Build(endpoint *url.URL) *HTTPHealthChec
 	policy := *hb.HTTPHealthCheckPolicy
 	policy.CircuitBreaker = builder.Build()
 
-	// change the initial state to half-open,
-	// so the first request will trigger the OnStateChanged event to push metrics.
-	policy.HalfOpen()
+	// Record initial metrics for the closed state.
+	metrics.ServerState.Record(
+		context.TODO(),
+		int64(circuitbreaker.ClosedState),
+		metricsAttrs,
+	)
 
 	return &policy
 }
