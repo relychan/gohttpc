e19de1fcc5fc9356da558425fdbf029f670f33d5
Toan Nguyen <hgiasac@gmail.com>
2025-12-10 01:00:23 +0700

    chore: upgrade gotel sdk (#17)

--- request.go
+++ request.go
@@ -24,7 +24,7 @@ import (
 	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/metric"
 	"go.opentelemetry.io/otel/propagation"
-	semconv "go.opentelemetry.io/otel/semconv/v1.30.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.38.0"
 	"go.opentelemetry.io/otel/trace"
 )
 
